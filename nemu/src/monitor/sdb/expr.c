cf28bcbb40c3bef00be881808b69bf13b4425de3
storm-1614 <storm1614@outlook.com>
2026-08-05 13:31:29 +0800

    feat: 支持十六进制数

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -20,12 +20,17 @@
  * Type 'man regex' for more information about POSIX regex functions.
  */
 #include <regex.h>
+#include <stdlib.h>
+#include <string.h>
 
 enum
 {
     TK_NOTYPE = 256, // 空格串
     TK_EQ,
-    TK_NUM, // 数字类型，十进制数字
+    TK_NUM,   // 数字类型，十进制数字
+    TK_DEREF, // * 解引用
+    TK_NEG,   // - 负号
+    TK_HEX,
 };
 
 static struct rule
@@ -34,20 +39,20 @@ static struct rule
     int token_type;
 } rules[] = {
 
-    /* 
+    /*
      * 规则按表从上到下匹配
      */
 
-    {" +", TK_NOTYPE}, // spaces
-    {"\\+", '+'},      // plus
-    {"\\-", '-'},      // minus
-    {"\\*", '*'},      // multiply
-    {"\\/", '/'},      // divison
-    {"\\(", '('},      // left bracket
-    {"\\)", ')'},      // right bracket
-    // 如果以后要支持十六进制，要把匹配项放在十进制前面
-    {"[0-9]+", TK_NUM}, // dec number
-    {"==", TK_EQ},      // equal
+    {" +", TK_NOTYPE},             // spaces
+    {"\\+", '+'},                  // plus
+    {"\\-", '-'},                  // minus
+    {"\\*", '*'},                  // multiply
+    {"\\/", '/'},                  // divison
+    {"\\(", '('},                  // left bracket
+    {"\\)", ')'},                  // right bracket
+    {"0[xX][0-9a-fA-F]+", TK_HEX}, // hex number
+    {"[0-9]+", TK_NUM},            // dec number
+    {"==", TK_EQ},                 // equal
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -117,6 +122,13 @@ static bool make_token(char *e)
                 {
                 case TK_NOTYPE:
                     break;
+                case TK_HEX:
+                    Assert(substr_len < 32, "溢出! substr_len=%d", substr_len);
+                    strncpy(tokens[nr_token].str, substr_start, substr_len);
+                    tokens[nr_token].str[substr_len] = '\0'; // 补结束符
+                    tokens[nr_token].type = TK_HEX;
+                    nr_token++;
+                    break;
                 case TK_NUM:
                     Assert(substr_len < 32, "溢出! substr_len=%d", substr_len);
                     strncpy(tokens[nr_token].str, substr_start, substr_len);
@@ -167,6 +179,8 @@ bool check_parentheses(int p, int q)
     return depth == 1;
 }
 
+/* 找主运算符
+ */
 int find_dominant_operator(int p, int q)
 {
     int op = -1;
@@ -208,10 +222,13 @@ int find_dominant_operator(int p, int q)
 int eval(int p, int q)
 {
     Assert(p <= q, "Bad expression");
-    if (p == q)
+    if (p == q) // 递归到只剩下一个词，仅剩下操作数
     {
-        Assert(tokens[p].type == TK_NUM, "Is not Number");
-        return (int)strtol(tokens[p].str, NULL, 10);
+        Assert(tokens[p].type == TK_NUM || tokens[p].type == TK_HEX, "Is not Number");
+        if (tokens[p].type == TK_NUM)
+            return (int)strtol(tokens[p].str, NULL, 10);
+        else // 16 进制
+            return (int)strtol(tokens[p].str, NULL, 16);
     }
 
     if (check_parentheses(p, q)) // 去除括号
@@ -241,10 +258,21 @@ int eval(int p, int q)
  */
 word_t expr(char *e, bool *success)
 {
+    int i;
     if (!make_token(e))
     {
         *success = false;
         return 0;
+    }
+
+    for (i = 0; i < nr_token; i++)
+    {
+        if (tokens[i].type == '*')
+        {
+            bool ismultiply = (i > 0 && (tokens[i - 1].type == ')' || tokens[i - 1].type == TK_NUM || tokens[i-1].type == TK_HEX));
+            if (!ismultiply)
+                tokens[i].type = TK_DEREF;
+        }
     }
 
     *success = true;
