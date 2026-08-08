bb0762d38741b3ed5f0d23ea7b07d4ee9d947d88
storm-1614 <storm1614@outlook.com>
2026-08-08 12:26:44 +0800

    feat: 实现 addi 和 jal 指令

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -14,6 +14,7 @@
  ***************************************************************************************/
 
 #include "local-include/reg.h"
+#include "macro.h"
 #include <cpu/cpu.h>
 #include <cpu/decode.h>
 #include <cpu/ifetch.h>
@@ -28,6 +29,7 @@ enum
     TYPE_U,
     TYPE_S,
     TYPE_N, // none
+    TYPE_J,
 };
 
 #define src1R()                                                                                                        \
@@ -55,6 +57,11 @@ enum
     {                                                                                                                  \
         *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7);                                                       \
     } while (0)
+#define immJ()                                                                                                         \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        *imm = SEXT(BITS(i, 31, 31), 1) << 20 | BITS(i, 30, 21) << 1 | BITS(i, 20, 20) << 11 | BITS(i, 19, 12) << 12;  \
+    } while (0)
 
 /* 按 type 规定的位布局，从机器码里把所有数值取出来，填进指针参数
  */
@@ -79,6 +86,9 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
         immS();
         break;
     case TYPE_N:
+        break;
+    case TYPE_J:
+        immJ();
         break;
     default:
         panic("unsupported type = %d", type);
@@ -106,6 +116,10 @@ static int decode_exec(Decode *s)
     INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb, S, Mw(src1 + imm, 1, src2));
 
     INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+
+    INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi, I, R(rd) = src1 + imm);
+    INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J, s->dnpc = s->pc + imm);
+
     INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
     INSTPAT_END();
 
