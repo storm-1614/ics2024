21d6bb9f7320bfabd051116764612077e6f39986
storm-1614 <storm1614@outlook.com>
2026-08-08 14:38:27 +0800

    feat: 适配 max.c

--- doc/pa2实验报告.md
+++ doc/pa2实验报告.md
@@ -122,3 +122,67 @@ case TYPE_J:
 ```
 
 就可以了。主要是看指令的类型，指令的用法。以及，要善用 gdb attach 进程，给程序打断点，看结构体数据什么的很好用。总的来说，难度中上。  
+
+## 适配 max.c
+### `lw` 指令
+``` asm
+80000070:	000aa903          	lw	s2,0(s5)
+```
+
+从设计之道 16 页找到格式和类型，填上之后到 [https://www.cnblogs.com/sureZ-learning/p/18402849](https://www.cnblogs.com/sureZ-learning/p/18402849) 找到 lw 的操作，得到：  
+
+``` c
+INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw, I, R(rd) = Mr(src1 + imm, 4));
+```
+
+### `bge` 指令
+``` asm
+80000084:	01255463          	bge	a0,s2,8000008c <main+0x64>
+```
+
+同上，但是 bge 是 B 类型指令。需要补下 immB 和 typeB。照着 immJ 改还是可以的：  
+
+``` c
+#define immB()                                                                                                         \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        *imm = SEXT(BITS(i, 31, 31), 1) << 12 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1 | BITS(i, 7, 7) << 11;      \
+    } while (0);
+```
+
+然后
+```c
+case TYPE_B:
+    src1R();
+    src2R();
+    immB();
+    break;
+```
+
+bge 判断是否`src1 >= src2` 如果是，就将 pc 加上立即数。反汇编把加好的地址 直接写了。  
+``` c
+INSTPAT("??????? ????? ????? 101 ????? 11000 11", bge, B, if (src1 >= src2) s->dnpc += imm);
+```
+
+有那么简单吗？  
+补完所有指令后跑起来最后结果是:  HIT BAD TRAP  
+问题就出来 bge:
+
+![](./res/bge.png)  
+
+bge 是有符号比较。所以 src1 和 src2 应该强制转换为有符号数：  
+``` c
+INSTPAT("??????? ????? ????? 101 ????? 11000 11", bge, B, if ((sword_t)src1 >= (sword_t)src2) s->dnpc = s->pc + imm);
+```
+这样才能通过。  
+
+### `sub` 指令
+### `seqz` 指令
+在设计之道的 163 页讲 seqz 展开为 sltiu。在 165 页讲了 sltiu 的逻辑。  
+``` c
+INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu, I, if (src1 < imm) R(rd) = 1; else R(rd) = 0); // seqz
+```
+
+…… 略  
+
+
--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -30,6 +30,8 @@ enum
     TYPE_S,
     TYPE_N, // none
     TYPE_J,
+    TYPE_B,
+    TYPE_R,
 };
 
 #define src1R()                                                                                                        \
@@ -62,6 +64,11 @@ enum
     {                                                                                                                  \
         *imm = SEXT(BITS(i, 31, 31), 1) << 20 | BITS(i, 30, 21) << 1 | BITS(i, 20, 20) << 11 | BITS(i, 19, 12) << 12;  \
     } while (0)
+#define immB()                                                                                                         \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        *imm = SEXT(BITS(i, 31, 31), 1) << 12 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1 | BITS(i, 7, 7) << 11;      \
+    } while (0)
 
 /* 按 type 规定的位布局，从机器码里把所有数值取出来，填进指针参数
  */
@@ -89,6 +96,15 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
         break;
     case TYPE_J:
         immJ();
+        break;
+    case TYPE_B:
+        immB();
+        src1R();
+        src2R();
+        break;
+    case TYPE_R:
+        src1R();
+        src2R();
         break;
     default:
         panic("unsupported type = %d", type);
@@ -118,9 +134,17 @@ static int decode_exec(Decode *s)
     INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
 
     INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi, I, R(rd) = src1 + imm);
-    INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr, I, R(rd) = s->pc + 4; s->dnpc = src1 + imm);
+    INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr, I, R(rd) = s->pc + 4; s->dnpc = src1 + imm); // ret
     INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J, R(rd) = s->pc + 4; s->dnpc = s->pc + imm);
     INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw, S, Mw(src1 + imm, 4, src2));
+
+    INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw, I, R(rd) = Mr(src1 + imm, 4));
+    INSTPAT("??????? ????? ????? 101 ????? 11000 11", bge, B,
+            if ((sword_t)src1 >= (sword_t)src2) s->dnpc = s->pc + imm);
+    INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub, R, R(rd) = src1 - src2);
+    INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu, I, if (src1 < imm) R(rd) = 1; else R(rd) = 0); // seqz
+    INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq, B, if (src1 == src2) s->dnpc = s->pc + imm);
+    INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne, B, if (src1 != src2) s->dnpc = s->pc + imm);
 
     INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
     INSTPAT_END();
