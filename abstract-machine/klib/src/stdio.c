4b5891e2b57109034ffd7af3a554c03e51cd85b5
storm-1614 <storm1614@outlook.com>
2026-08-11 10:58:40 +0800

    clean stdio.c

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,6 +1,6 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <limits.h>
 #include <stdarg.h>
 
@@ -50,15 +50,18 @@ void write_str(char *buf, int *len, int cap, const char *s)
         writec(buf, len, cap, *s);
 }
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+int printf(const char *fmt, ...)
+{
+    panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
     return vsnprintf(out, INT_MAX, fmt, ap);
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int sprintf(char *out, const char *fmt, ...)
+{
     int len;
     va_list ap;
     va_start(ap, fmt);
@@ -67,18 +70,20 @@ int sprintf(char *out, const char *fmt, ...) {
     return len;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
     int len;
     va_list ap;
     va_start(ap, fmt);
-    len = vsnprintf(out, n,fmt, ap);
+    len = vsnprintf(out, n, fmt, ap);
     va_end(ap);
     return len;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
     int len = 0;
-    for(;*fmt != '\0';fmt++)
+    for (; *fmt != '\0'; fmt++)
     {
         if (*fmt != '%')
         {
@@ -94,7 +99,7 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
             writec(out, &len, n, '%');
     }
     // 考虑被截断可能的写结束符
-    int end = len < (int)n ? len:(int) n - 1;
+    int end = len < (int)n ? len : (int)n - 1;
     if (n > 0)
         out[end] = '\0';
 
