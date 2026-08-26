81a3f905cc15221ef75fd01c44f03615bb491e2f
storm-1614 <storm1614@outlook.com>
2026-08-26 20:27:10 +0800

    feat: VGA 视频实现

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,23 +3,50 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {
+static int screen_w = 0, screen_h = 0;
+
+/*
+ * 读取屏幕宽高
+ */
+void __am_gpu_init()
+{
+    uint32_t vgactl = inl(VGACTL_ADDR);
+    screen_w = vgactl >> 16;
+    screen_h = vgactl & 0xffff;
 }
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+/*
+ * 初始化 gpu 数据
+ */
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
+{
+    *cfg = (AM_GPU_CONFIG_T){.present = true,
+                             .has_accel = false,
+                             .width = screen_w,
+                             .height = screen_h,
+                             .vmemsz = screen_w * screen_h * sizeof(uint32_t)};
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+/*
+ * 将像素块写进帧缓冲
+ */
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
+{
+    uint32_t *fb = (uint32_t *)FB_ADDR;
+    for (int i = 0; i < ctl->h; i++)
+    {
+        for (int j = 0; j < ctl->w; j++)
+        {
+            fb[(ctl->y + i) * screen_w + (ctl->x + j)] = ((uint32_t *)ctl->pixels)[i * ctl->w + j];
+        }
+    }
+    if (ctl->sync)
+    {
+        outl(SYNC_ADDR, 1); // 刷新屏幕
+    }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
+void __am_gpu_status(AM_GPU_STATUS_T *status)
+{
+    status->ready = true;
 }
--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -13,12 +13,12 @@
 # See the Mulan PSL v2 for more details.
 #**************************************************************************************/
 
-ifeq ($(CONFIG_ITRACE)$(CONFIG_IQUEUE),)
-SRCS-BLACKLIST-y += src/utils/disasm.c
-else
+#ifeq ($(CONFIG_ITRACE)$(CONFIG_IQUEUE),)
+#SRCS-BLACKLIST-y += src/utils/disasm.c
+#else
 LIBCAPSTONE = tools/capstone/repo/libcapstone.so.5
 CFLAGS += -I tools/capstone/repo/include
 src/utils/disasm.c: $(LIBCAPSTONE)
 $(LIBCAPSTONE):
 	$(MAKE) -C tools/capstone
-endif
+#endif
